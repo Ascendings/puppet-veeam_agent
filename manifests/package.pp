eb96181daca68f93049bccc1e8767064a121fc09
Gregory Ballantine <brotherballantine@gmail.com>
2017-01-27 16:54:13 -0500

    Added support for Fedora and multiple versions of CentOS/Redhat

--- manifests/package.pp
+++ manifests/package.pp
@@ -4,29 +4,59 @@ class veeam_agent::package (
   $manage_repo = $::veeam_agent::manage_repo,
   $repo_name = $::veeam_agent::repo_name,
   $repo_ensure = $::veeam_agent::repo_ensure,
+  $ensure_epel = true,
 
   # package stuff
   $package_name = $::veeam_agent::package_name,
   $package_ensure = $::veeam_agent::package_ensure,
 ){
-  if $manage_repo {
-    yumrepo { 'veeam_repo':
-      name     => $repo_name,
-      ensure   => $repo_ensure,
-      descr    => 'Veeam Backup for GNU/Linux - $basearch',
-      baseurl  => 'http://repository.veeam.com/backup/linux/agent/rpm/el/7/x86_64',
-      enabled  => true,
-      gpgcheck => true,
-      gpgkey   => 'http://repository.veeam.com/keys/RPM-GPG-KEY-VeeamSoftwareRepo http://repository.veeam.com/keys/VeeamSoftwareRepos'
+  case $::osfamily {
+    'redhat': {
+      case $::operatingsystem {
+        'redhat', 'centos': {
+          if $manage_repo {
+            yumrepo { 'veeam_repo':
+              ensure   => $repo_ensure,
+              name     => $repo_name,
+              descr    => 'Veeam Backup for GNU/Linux - $basearch',
+              baseurl  => "http://repository.veeam.com/backup/linux/agent/rpm/el/${::operatingsystemmajrelease}/\$basearch",
+              enabled  => true,
+              gpgcheck => true,
+              gpgkey   => 'http://repository.veeam.com/keys/RPM-GPG-KEY-VeeamSoftwareRepo http://repository.veeam.com/keys/VeeamSoftwareRepos'
+            }
+          }
+        }
+        'fedora': {
+          if $manage_repo {
+            yumrepo { 'veeam_repo':
+              ensure   => $repo_ensure,
+              name     => $repo_name,
+              descr    => 'Veeam Backup for GNU/Linux - $basearch',
+              baseurl  => "http://repository.veeam.com/backup/linux/agent/rpm/fc/${::operatingsystemmajrelease}/\$basearch",
+              enabled  => true,
+              gpgcheck => true,
+              gpgkey   => 'http://repository.veeam.com/keys/RPM-GPG-KEY-VeeamSoftwareRepo http://repository.veeam.com/keys/VeeamSoftwareRepos'
+            }
+          }
+        }
+        default: {
+          fail("${::operatingsystem} is not supported yet!")
+        }
+      }
+
+      if $ensure_epel {
+        # make sure epel is installed
+        ensure_packages(['epel-release'])
+      }
+
+      package { 'veeam_package':
+        ensure  => $package_ensure,
+        name    => $package_name,
+        require => [ Yumrepo['veeam_repo'], Package['epel-release'], ],
+      }
     }
-  }
-
-  # make sure epel and dkms are installed
-  ensure_packages(['epel-release'])
-
-  package { 'veeam_package':
-    name    => $package_name,
-    ensure  => $package_ensure,
-    require => [ Yumrepo['veeam_repo'], Package['epel-release'], ],
+    default: {
+      fail("${::operatingsystem} is not supported yet!")
+    }
   }
 }
